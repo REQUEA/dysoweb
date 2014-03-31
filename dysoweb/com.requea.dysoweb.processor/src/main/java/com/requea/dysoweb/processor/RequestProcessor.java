377a6590d1bb324271895cf28c65ff17011927ce
pdubois22 <pdubois@requea.com>
2014-03-31 16:51:53 +0000

    renum core / BEA
    git-svn-id: https://svnlegacy.ow2.org/dysoweb/trunk@339 324d2c6f-2539-0410-b2fb-80a91360cbc7

--- dysoweb/com.requea.dysoweb.processor/src/main/java/com/requea/dysoweb/processor/RequestProcessor.java
+++ dysoweb/com.requea.dysoweb.processor/src/main/java/com/requea/dysoweb/processor/RequestProcessor.java
@@ -1499,7 +1499,7 @@ public class RequestProcessor implements IWebProcessor {
 			// for BEA WLS, that is another story 
 			try {
 				// add at least the core jar file to compile taglibs
-				URL coreJar = fServletContext.getResource("/WEB-INF/lib/dysoweb-core-1.0.26.jar");
+				URL coreJar = fServletContext.getResource("/WEB-INF/lib/dysoweb-core-1.0.34.jar");
 				parentURL = new URL[] { coreJar };
 			} catch (MalformedURLException e) {
 				// use a default URL
