9fcf78e7d20f9c88e0f4cb807ad4b6237bef0c95
pdubois22 <pdubois@requea.com>
2016-02-24 09:42:58 +0000

    update core api number for bea weblo
    git-svn-id: https://svnlegacy.ow2.org/dysoweb/trunk@400 324d2c6f-2539-0410-b2fb-80a91360cbc7

--- dysoweb/com.requea.dysoweb.processor/src/main/java/com/requea/dysoweb/processor/RequestProcessor.java
+++ dysoweb/com.requea.dysoweb.processor/src/main/java/com/requea/dysoweb/processor/RequestProcessor.java
@@ -1545,7 +1545,7 @@ public class RequestProcessor implements IWebProcessor {
 			// for BEA WLS, that is another story 
 			try {
 				// add at least the core jar file to compile taglibs
-				URL coreJar = fServletContext.getResource("/WEB-INF/lib/dysoweb-core-1.0.34.jar");
+				URL coreJar = fServletContext.getResource("/WEB-INF/lib/dysoweb-core-1.0.45.jar");
 				parentURL = new URL[] { coreJar };
 			} catch (MalformedURLException e) {
 				// use a default URL
